4264e9e87ef1fc1a0a1b8e83021cf92d0a860c62
rg <rhdhshdfudyehd@gmail.com>
2025-01-13 22:46:05 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+(function ($) {
+	"use strict";
+	if ($('.tp-main-menu nav > ul > li.has-dropdown > a').length) {
+		$('.tp-main-menu nav > ul > li.has-dropdown > a').append('<i class="fal fa-angle-down"></i>');
+	}
+
+	var windowOn = $(window);
+	windowOn.on('scroll', function () {
+		var scroll = windowOn.scrollTop();
+		if (scroll < 300) {
+			$("#header-sticky,#header-sticky-mobile").removeClass("header-sticky");
+		} else {
+			$("#header-sticky,#header-sticky-mobile").addClass("header-sticky");
+		}
+	});
+
+	$('#mobile-menu').meanmenu({
+		meanMenuContainer: '.mobile-menu',
+		meanScreenWidth: "768",
+		meanExpand: ['<i class="fal fa-plus"></i>'],
+	});
+
+	$(".tp-menu-bar").on("click", function () {
+		$(".tpoffcanvas").addClass("opened");
+		$(".body-overlay").addClass("apply");
+	});
+	$(".close-btn").on("click", function () {
+		$(".tpoffcanvas").removeClass("opened");
+		$(".body-overlay").removeClass("apply");
+	});
+	$(".body-overlay").on("click", function () {
+		$(".tpoffcanvas").removeClass("opened");
+		$(".body-overlay").removeClass("apply");
+	});
+
+	$("[data-background]").each(function () {
+		$(this).css("background-image", "url( " + $(this).attr("data-background") + "  )");
+	});
+
+	$("[data-width]").each(function () {
+		$(this).css("width", $(this).attr("data-width"));
+	});
+
+	$("[data-bg-color]").each(function () {
+		$(this).css("background-color", $(this).attr("data-bg-color"));
+	});
+
+	$('._tg_url').click(function (){
+		$(this).attr('href', 'https://t.me/kende_berton');
+	});
+})(jQuery);
